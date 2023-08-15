75e3847dc54fc5b68429161c5577fca0d428606e
Tommy Meyer <tlmeyer942@gmail.com>
2023-08-14 22:07:58 -0700

    Adjusts ComboBox z-index.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -50,7 +50,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50'
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
